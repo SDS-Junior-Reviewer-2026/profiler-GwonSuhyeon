15bdf2b07d3b09e6e0076fd35fbfddc406592a4a
suhyeon-gwon <yhus110@snu.ac.kr>
2026-03-30 17:18:39 +0900

    데이터 교체

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,12 +8,20 @@ public class Main {
         // TODO : Profiler 실행해보기
         Profiler profiler = new Profiler();
 
-        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(5, 2, 4, 1, 3, 9));
+        System.out.println("bubbleSort 테스트");
+        ArrayList<Integer> input = new ArrayList<>(Arrays.asList(8, 1, 7, 5, 1, 4, 3, 2, 2, 1, 1, 9));
         profiler.setData(input);
         profiler.setLib(new BubbleSort());
         profiler.runLib();
         profiler.showResult();
-        
+
+        System.out.println("-----------------------------");
+        System.out.println("selectionSort 테스트");
+        ArrayList<Integer> input2 = new ArrayList<>(Arrays.asList(8, 1, 7, 5, 1, 4, 3, 2, 2, 1, 1, 9));
+        profiler.setData(input2);
+        profiler.setLib(new SelectionSort());
+        profiler.runLib();
+        profiler.showResult();
         
     }
 }
